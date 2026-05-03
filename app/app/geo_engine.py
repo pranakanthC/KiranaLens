6e9ff2bc8b88cdd2a40a4f0868ee6980d95e2022
pranakanthC <142805087+pranakanthC@users.noreply.github.com>
2026-05-03 12:55:58 +0530

    Delete app directory

--- app/app/geo_engine.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-class GeoEngine:
- def get(self,lat,lng):
-  h=int(hashlib.md5(f"{round(lat,2)}{round(lng,2)}".encode()).hexdigest(),16)
-  c=int((h%20)+5)
-  f=min(c*0.1,0.9)
-  comp=0.7 if c<5 else 0.4
-  return round(0.35*0.5+0.4*f+0.25*comp,4)
